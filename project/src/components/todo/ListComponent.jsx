d13462c19da350c4d28576753de1b5ee9e0c0eb4
doolgi158 <kolonel@hanmail.net>
2025-09-25 16:37:58 +0900

    수업 끝

--- project/src/components/todo/ListComponent.jsx
+++ project/src/components/todo/ListComponent.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import useCustomMove from "../../hooks/useCustomMove";
 import { getList } from "../../api/todoApi";
 import PageComponent from "../common/PageComponent";
+import useCustomLogin from "../../hooks/useCustomLogin";
 
 const initState = {
     dtoList: [],
@@ -18,15 +19,15 @@ const initState = {
 
 const ListComponent = () => {
     const {page, size, moveToList, moveToRead, refresh} = useCustomMove();
+    const {exceptionHandle} = useCustomLogin();
 
-    // serverData는 나중에 사용
     const [serverData, setServerData] = useState(initState);
-
+    
     useEffect(() => {
         getList({page, size}).then(data => {
             console.log(data)
             setServerData(data)
-        })
+        }).catch(err => exceptionHandle(err));
     }, [page, size, refresh]);
 
     return (
--- project/src/util/jwtUtil.js
+++ project/src/util/jwtUtil.js
@@ -1,7 +1,22 @@
 import axios from "axios";
-import { getCookie } from "./cookieUtil";
+import { getCookie, setCookie } from "./cookieUtil";
+import { API_SERVER_HOST } from "../api/todoApi";
 
 const jwtAxios = axios.create();
+
+const refreshJWT = async (accessToken, refreshToken) => {
+    const host = API_SERVER_HOST;
+    const header = {headers: {"Authorization": `Bearer ${accessToken}`}}
+
+    const res = await axios.get(
+        `${host}/api/member/refresh?refreshToken=${refreshToken}`,
+        header
+    );
+    console.log("------------------------------------");
+    console.log(res.data);
+
+    return res.data;
+}
 
 // before request
 const beforeReq = (config) => {
@@ -35,6 +50,28 @@ const requestFail = (err) => {
 // before return response
 const beforeRes = async(res) => {
     console.log("before return response...");
+    console.log(res);
+    const data = res.data;
+
+    if(data && data.error === 'ERROR_ACCESS_TOKEN') {
+        const memberCookieValue = getCookie("member");
+        const result = await refreshJWT(
+            memberCookieValue.accessToken,
+            memberCookieValue.refreshToken
+        );
+
+        console.log("refreshJWT RESULT", result);
+        memberCookieValue.accessToken = result.accessToken;
+        memberCookieValue.refreshToken = result.refreshToken;
+
+        setCookie("member", JSON.stringify(memberCookieValue), 1);
+
+        // 원래의 호출
+        const originalRequest = res.config;
+        originalRequest.headers.Authorization = `Bearer ${result.accessToken}`;
+        return await axios(originalRequest);
+    }
+    
     return res;
 };
 
